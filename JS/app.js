588ec9c81944e0b1ddb51c4c9bc2f84b345a974b
DUTTARGI-PAVAN <pavanduttargi10m.05@gmail.com>
2026-02-16 23:05:01 +0530

    3 commit

--- JS/app.js
+++ JS/app.js
@@ -6,6 +6,8 @@ const expense = document.getElementById("expense");
 
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
+let chart;
+
 function updateUI() {
 
     list.innerHTML = "";
@@ -17,18 +19,18 @@ function updateUI() {
     transactions.forEach((t, index) => {
 
         const li = document.createElement("li");
+
         li.innerHTML = `
-            ${t.text} : ₹${t.amount}
+            ${t.text} (${t.category}) : ₹${t.amount}
             <button onclick="deleteTransaction(${index})">X</button>
         `;
 
         list.appendChild(li);
 
-        total += Number(t.amount);
+        total += t.amount;
 
-        if (t.amount > 0) inc += Number(t.amount);
-        else exp += Number(t.amount);
-
+        if (t.amount > 0) inc += t.amount;
+        else exp += t.amount;
     });
 
     balance.innerText = total;
@@ -36,6 +38,8 @@ function updateUI() {
     expense.innerText = Math.abs(exp);
 
     localStorage.setItem("transactions", JSON.stringify(transactions));
+
+    updateChart(inc, Math.abs(exp));
 }
 
 function deleteTransaction(index) {
@@ -48,15 +52,39 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const text = document.getElementById("text").value;
-    const amount = document.getElementById("amount").value;
+    const amount = Number(document.getElementById("amount").value);
+    const category = document.getElementById("category").value;
 
     transactions.push({
         text,
-        amount: Number(amount)
+        amount,
+        category
     });
 
     updateUI();
     form.reset();
+});
+
+function updateChart(incomeValue, expenseValue) {
+
+    const ctx = document.getElementById("myChart").getContext("2d");
+
+    if (chart) chart.destroy();
+
+    chart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+            labels: ["Income", "Expense"],
+            datasets: [{
+                data: [incomeValue, expenseValue]
+            }]
+        }
+    });
+}
+const toggleBtn = document.getElementById("toggleMode");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
 });
 
 updateUI();
